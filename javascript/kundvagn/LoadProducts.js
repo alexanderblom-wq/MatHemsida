29709fe22ea01d268d853b8265a950ad4e48b6ec
Alexander Blom <alexander.blom@wraps.se>
2026-05-18 08:44:33 +0200

    Update LoadProducts.js

--- javascript/kundvagn/LoadProducts.js
+++ javascript/kundvagn/LoadProducts.js
@@ -1,11 +1,16 @@
 import express from 'express';
 const router = express.Router();
 
-router.get('/api/products', async (req, res) => {
+
+router.get('/menu', async (req, res) => {
     try {
         const result = await req.db.query('SELECT * FROM mat_ratter');
         const products = result.rows;
-        res.json(products); // Skickar datan som JSON till frontend
+        // create div for each product and append to menu
+        
+        
+
+
     } catch (error) {
         console.error('Fel vid hämtning av produkter:', error);
         res.status(500).json({ error: 'Kunde inte hämta produkter' });
