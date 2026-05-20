dd609ed0fa649da17cf3d63bb929846b3ece848e
kevinjangwang <kevinjangwang@gmail.com>
2026-05-20 16:16:49 +0200

    login för admin

--- javascript/login/login.js
+++ javascript/login/login.js
@@ -5,7 +5,14 @@ import bcrypt from 'bcrypt';
 const loginRoutes = express.Router();
 
 loginRoutes.post('/login', async (req, res) => {
+const ADMIN_USERNAME = process.env.ADMIN_USERNAME;
+const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
   const { email, password } = req.body;
+  if (email === ADMIN_USERNAME && password === ADMIN_PASSWORD) {
+    req.session.userId  = 'admin';
+    req.session.isAdmin = true;
+    return res.redirect('/admin');
+}
     try {
         const result = await req.db.query('SELECT * FROM users WHERE email = $1', [email]);
         if (result.rows.length === 0) {
